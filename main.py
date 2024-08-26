1f047b4cf5c59acd8186c721e05e74ca7c6cb4ef
manemsai <manemmohan.217@gmail.com>
2024-08-25 23:16:57 -0400

    data ingestion notebook added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from text_Summerizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from text_Summerizer.logging import logger
 
-logger.info("welcome to our custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
